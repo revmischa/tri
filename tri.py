7875b62b79e57c748882314f369abfceab5f40cb
Mischa S <revmischa@cpan.org>
2013-12-25 17:42:24 -0800

    mostly working, need to figure out why still overlapping

--- tri.py
+++ tri.py
@@ -3,13 +3,21 @@
 
 import sys
 import random
-from math import pi
+from random import choice, shuffle
+from math import pi, sin
+from collections import namedtuple
 
 trisize = 0.05
-gentriframes = 5  # new tri every X frames
+triscale = 0.85
+zrange = 0.5
+rotspeed = .4
+gentriframes = 3  # new tri every X frames
+screenwidth = 1280
+screenheight = 800
 
 try:
     from OpenGL.GL import *
+    import OpenGL.GLU as glu
     from sdl2 import *
     import sdl2.ext as sdl2ext
 except ImportError:
@@ -17,31 +25,58 @@ except ImportError:
     traceback.print_exc()
     sys.exit(1)
 
+TriTup = namedtuple("TriTup", ['x', 'y', 'ang'])
+
+# find a node with no children
+def find_random_leaf(root):
+    children = root.trichildren.children
+    shuffle(children)
+    if len(children):
+        # pick random child, return that or one of its children
+        shuffle(children)
+        for child in children:
+            if len(child.trichildren.children):
+                return find_random_leaf(child)
+            else:
+                return child
+    else:
+        print "returning root"
+        return root
+
 class TriGenerator(sdl2ext.Applicator):
     def __init__(self):
         super(TriGenerator, self).__init__()
         self.componenttypes = [ TriPos, TriColor ]
-        self.framecount = 0
         self.initial = None
+        self.existing = {}
+        self.neednewleaf = False
     def process(self, world, componentsets):        
-        # generate initial
+        # generate root
         if not self.initial:
             self.initial = Tri(world)
             self.lasttri = self.initial
             return
 
         # append new
-        self.framecount += 1
-        if self.initial == self.lasttri or (self.framecount % gentriframes == 0):
+        if world.framecount % gentriframes == 0:
+            if self.neednewleaf:
+                self.lasttri = find_random_leaf(self.initial)
+                if not self.lasttri:
+                    print "failed to find new leaf"
+                    return
+                self.neednewleaf = False
+
             last = self.lasttri
+
             lastpos = last.tripos
             ang = lastpos.ang
 
-            # get existing child angles
+            # get valid angles for new tri
             seenang = [ ang ]
             # don't want same angle as parent
             if last.triparentang.ang != None:
                 seenang.append(last.triparentang.ang)
+            # get existing child angles
             for child in last.trichildren.children:
                 seenang.append(child.tripos.ang)
             if len(seenang) > 4:
@@ -54,46 +89,28 @@ class TriGenerator(sdl2ext.Applicator):
                 newang = random.randint(0, 3) * 90
 
             # position new child according to angles of parent and child
+            x = lastpos.x
+            y = lastpos.y
             if ang == 0:
                 if newang == 90:
-                    x = lastpos.x
-                    y = lastpos.y - trisize*2
-                elif newang == 180:
-                    x = lastpos.x
-                    y = lastpos.y
+                    x -= trisize*2
                 elif newang == 270:
-                    x = lastpos.x - trisize*2
-                    y = lastpos.y
+                    y -= trisize*2
             elif ang == 90:
                 if newang == 0:
-                    x = lastpos.x
-                    y = lastpos.y + trisize*2
+                    x += trisize*2
                 elif newang == 180:
-                    x = lastpos.x - trisize*2
-                    y = lastpos.y
-                elif newang == 270:
-                    x = lastpos.x
-                    y = lastpos.y
+                    y -= trisize*2
             elif ang == 180:
-                if newang == 0:
-                    x = lastpos.x
-                    y = lastpos.y
-                elif newang == 90:
-                    x = lastpos.x + trisize*2
-                    y = lastpos.y
+                if newang == 90:
+                    y += trisize*2
                 elif newang == 270:
-                    x = lastpos.x + trisize*2
-                    y = lastpos.y
+                    x += trisize*2
             elif ang == 270:
                 if newang == 0:
-                    x = lastpos.x + trisize*2
-                    y = lastpos.y
-                elif newang == 90:
-                    x = lastpos.x + trisize*2
-                    y = lastpos.y
+                    y += trisize*2
                 elif newang == 180:
-                    x = lastpos.x
-                    y = lastpos.y + trisize*2
+                    x -= trisize*2
             else:
                 print "unhandled angle: %s" % newang
 
@@ -101,9 +118,22 @@ class TriGenerator(sdl2ext.Applicator):
                 # reset
                 resetnodes(self.initial)
                 self.initial = None
+                self.existing = {}
                 return
 
-            z = lastpos.z
+            # does something already exist in dest?
+            tt = TriTup(x=x, y=y, ang=newang)
+            # don't allow partial overlap
+            ttl = TriTup(x=x, y=y, ang=((newang+90) % 360))
+            ttr = TriTup(x=x, y=y, ang=((newang-90) % 360))
+            ttf = TriTup(x=x, y=y, ang=((newang+180) % 360))
+            if self.existing.has_key(tt) or self.existing.has_key(ttl) or self.existing.has_key(ttr):
+                self.neednewleaf = True
+                return
+            self.existing[tt] = True
+
+            z = random.uniform(0, 0.05)
+            # z = lastpos.z
             t = Tri(world, x, y, z, newang, ang);
             last.trichildren.children.append(t)
             if len(last.trichildren.children) >= 2:
@@ -138,67 +168,77 @@ class TriChildren(object):
 class TriRenderer(object):
     def __init__(self):
         self.addedtime = SDL_GetTicks()
-    def draw(self, pos, color):
+    def draw(self, world, pos, color, children):
         # cubic fade in
         if color.a < 1.0:
-            t = (SDL_GetTicks() - self.addedtime) / 150.0
+            t = (SDL_GetTicks() - self.addedtime) / 300.0
             color.a = 0.001*(t*t*t + 1)
         glPushMatrix()
 
-        glTranslatef(pos.x, pos.y, pos.z)
+        zscale = (sin(world.framecount / 10.0)+1.01)/2.0 * 4
+        glTranslatef(pos.x, pos.y, -0.9)
         glRotatef(pos.ang, 0, 0, 1)
-        glScalef(0.90, 0.90, 0.90)
+        glScalef(triscale, triscale, triscale)
         glBegin(GL_TRIANGLES)
+        # glColor4f(color.r / (1+len(children.children)), 0.1, 0.1, color.a)
         glColor4f(color.r, color.g, color.b, color.a)
 
-        glVertex3f(-trisize, trisize, 0)
-        glVertex3f(-trisize, -trisize, 0)
-        glVertex3f(trisize, -trisize, 0)
+        glVertex3f(-trisize, trisize, zscale*pos.z)
+        glVertex3f(-trisize, -trisize, zscale*pos.z)
+        glVertex3f(trisize, -trisize, zscale*pos.z)
 
         glEnd()
         glPopMatrix()
 
 class Tri(sdl2ext.Entity):
-    def __init__(self, world, posx=0, posy=0, posz=0, ang=0, parentang=None):
-        r = random.uniform(0.8, 1.0)
-        g = random.uniform(0.8, 1.0)
-        b = random.uniform(0.1, 0.2)
+    def __init__(self, world, posx=0.0, posy=0.0, posz=0.0, ang=0.0, parentang=None):
+        rmin = abs(sin(world.framecount / 10) / 3.0) + .3
+        r = random.uniform(rmin, rmin+0.1)
+        g = random.uniform(0.0, 0.1)
+        b = random.uniform(0.0, 0.1)
         self.tricolor = TriColor(r, g, b, 0.0001)
         self.tripos = TriPos(posx, posy, posz, ang)
         self.trirenderer = TriRenderer()
         self.trichildren = TriChildren()
         self.triparentang = TriParentAng(parentang)
 
+class TriWorld(sdl2ext.World):
+    def __init__(self):
+        self.framecount = 0.0
+
 class WorldRenderer(sdl2ext.Applicator):
     def __init__(self, window):
         super(WorldRenderer, self).__init__()
-        self.componenttypes = [ TriRenderer, TriPos, TriColor ]
+        self.componenttypes = [ TriRenderer, TriPos, TriColor, TriChildren ]
         self.window = window
-        self.rotate_x_speed = 0.01
-        self.rotate_y_speed = 0.01
-        self.rotate_z_speed = 1.0
+        self.rotate_x_speed = 0.01 * rotspeed
+        self.rotate_y_speed = 0.20 * rotspeed
+        self.rotate_z_speed = 0.50 * rotspeed
 
         # init gl
-        # SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8)
         SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
         SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES,16);
         self.glcontext = SDL_GL_CreateContext(window.window)
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        glLoadIdentity()
-
     def __del__(self):
         super(WorldRenderer, self)
         SDL_GL_DeleteContext(self.glcontext)
-
     def process(self, world, componentsets):
+        world.framecount += 1.0
+        fovy = 60 + sin(world.framecount / 150.0)*15
+
+        glLoadIdentity()
+        glu.gluPerspective(fovy, float(screenwidth)/float(screenheight), 0.01, 2);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        # glMatrixMode(GL_PROJECTION | GL_MODELVIEW)
+        glMatrixMode(GL_MODELVIEW)
+
         glPushMatrix()
         ticks = SDL_GetTicks()
-        glRotatef(ticks/300.0, self.rotate_x_speed, self.rotate_y_speed, self.rotate_z_speed)
-        for renderer, pos, color in componentsets:
-            renderer.draw(pos, color)
+        glRotatef(ticks/100.0, self.rotate_x_speed, self.rotate_y_speed, self.rotate_z_speed)
+        # render each tri
+        for renderer, pos, color, children in componentsets:
+            renderer.draw(world, pos, color, children)
         glPopMatrix()
         SDL_GL_SwapWindow(self.window.window)
 
@@ -206,13 +246,14 @@ def run():
     sdl2ext.init()
 
     # init window
-    window = sdl2ext.Window("tri", size=(1280, 800), flags=SDL_WINDOW_OPENGL|SDL_WINDOW_FULLSCREEN_DESKTOP)
+    window = sdl2ext.Window("tri", size=(screenwidth, screenheight), flags=SDL_WINDOW_OPENGL)#|SDL_WINDOW_FULLSCREEN_DESKTOP)
     window.show()
 
     worldrenderer = WorldRenderer(window)
     trigen = TriGenerator()
 
     world = sdl2ext.World()
+    world.framecount = 0
     world.add_system(worldrenderer)
     world.add_system(trigen)
 
